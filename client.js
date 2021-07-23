4cffe598478644716373541e66c44ef3caf4677a
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-07-22 21:22:29 -0500

    .append added

--- client.js
+++ client.js
@@ -1,5 +1,28 @@
 $(document).ready(onReady);
 
 function onReady(){
+    console.log('Ready');
 
+    $(document).on('click', '#submit', addToForm)
+}
+
+function addToForm(){
+    console.log('Button works');
+
+    let inputFirstName = $('#firstName').val();
+    let inputLastName = $('#lastName').val();
+    let inputId = $('#inputId').val();
+    let inputTitle = $('#title').val();
+    let inputAnnualSalary = $('#annualSalary').val();
+    console.log(inputFirstName, inputLastName, inputId, inputTitle, inputAnnualSalary)
+
+    $('form').append(`
+        <label><br>
+            ${inputFirstName}
+            ${inputLastName}
+            ${inputId}
+            ${inputTitle}
+            ${inputAnnualSalary}
+        </label>
+    `)
 }
