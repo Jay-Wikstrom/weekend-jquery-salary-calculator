cc13e8819fa6ef5f84d3b336b0ca9b5f4c5c654a
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-07-22 22:04:12 -0500

    Total Monthly halfway complete

--- client.js
+++ client.js
@@ -3,19 +3,23 @@ $(document).ready(onReady);
 function onReady(){
     console.log('Ready');
 
-    $(document).on('click', '#submit', addToForm)
+    $(document).on('click', '#submit', addToForm);
+    $(document).on('click', '#deleteButton', deleteInput);
+}
+
+function deleteInput(){
+    $(this).parent().remove();
 }
 
 function addToForm(){
-    console.log('Button works');
-
     let inputFirstName = $('#firstName').val();
     let inputLastName = $('#lastName').val();
     let inputId = $('#inputId').val();
     let inputTitle = $('#title').val();
     let inputAnnualSalary = $('#annualSalary').val();
-    console.log(inputFirstName, inputLastName, inputId, inputTitle, inputAnnualSalary)
+    //console.log(inputFirstName, inputLastName, inputId, inputTitle, inputAnnualSalary)
 
+    //Append input to form
     $('form').append(`
         <label><br>
             ${inputFirstName}
@@ -23,6 +27,32 @@ function addToForm(){
             ${inputId}
             ${inputTitle}
             ${inputAnnualSalary}
+            <button class="deleteButton">Delete</button>
         </label>
+
     `)
+
+
+    let addMonthly = 0;
+    addMonthly = addMonthly + parseInt(inputAnnualSalary);
+    console.log(addMonthly);
+
+    //Clear input
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#inputId').val('');
+    $('#title').val('');
+    $('#annualSalary').val('');
+
+
+    
+    //totalMonthly();
 }
+
+/*
+function totalMonthly(){
+    let addMonthly = 0;
+    addMonthly = addMonthly + parseInt(inputAnnualSalary);
+    console.log(addMonthly);
+}
+*/
