0ec4460ded2ff786b54ae5293256ca6f8630178b
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-07-23 19:18:51 -0500

    Update append + add object

--- client.js
+++ client.js
@@ -6,40 +6,55 @@ function onReady() {
     console.log('Ready');
 
     $(document).on('click', '#submit', addToForm);
-    $(document).on('click', '#deleteButton', deleteInput);
+    //$(document).on('click', '#deleteButton', deleteInput);
 }
-
+/*
 function deleteInput() {
     console.log('delete test');
     $(this).parent().remove();
-}
+}*/
 
 function addToForm() {
+
+
+    let salaryCalculator = {
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        id: $('#inputId').val(),
+        title: $('#title').val(),
+        annualSalary: $('#annualSalary').val()
+    }
+
+    /*
     let inputFirstName = $('#firstName').val();
     let inputLastName = $('#lastName').val();
     let inputId = $('#inputId').val();
     let inputTitle = $('#title').val();
     let inputAnnualSalary = $('#annualSalary').val();
+    */
     
 
     $('tbody').append(`
         <tr>
-            <td>${inputFirstName}</td>
-            <td>${inputLastName}</td>
-            <td>${inputId}</td>
-            <td>${inputTitle}</td>
-            <td>${inputAnnualSalary}</td>
+            <td>${salaryCalculator.firstName}</td>
+            <td>${salaryCalculator.lastName}</td>
+            <td>${salaryCalculator.id}</td>
+            <td>${salaryCalculator.title}</td>
+            <td>${salaryCalculator.annualSalary}</td>
         </tr>
     `)
     
     totalMonthly();
 
+    /*
     //Clear input
     $('#firstName').val('');
     $('#lastName').val('');
     $('#inputId').val('');
     $('#title').val('');
     $('#annualSalary').val('');
+    */
+    $('input').val('')
 
 }
 
